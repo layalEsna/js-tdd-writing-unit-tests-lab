f880466d48c114d7ff68c4654f134325376c82a3
Amene <layla_1380@yahoo.com>
2024-06-20 15:34:32 -0500

    isPalindrome test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,35 @@
 // Your tests here
+
+import { isPalindrome } from '../utils'
+
+describe('isPalindrome', () => {
+    it('returns true if the string is palindrome',
+        () => {
+            const word1 = 'mom'
+            const result = isPalindrome(word1)
+            expect(result).toBe(true)
+            
+        })
+        it('returns false if the string is not palindrome',
+        () => {
+            const word1 = 'door'
+            const result = isPalindrome(word1)
+            expect(result).toBe(false)
+            
+            })
+            it('returns true if the string is palindrome and contains lower case and upper case',
+            () => {
+                const word1 = 'Racecar'
+                const result = isPalindrome(word1)
+                expect(result).toBe(true)
+                
+                })
+                it("throws an error if the input is an empty string", () => {
+                    const word = "";
+                
+                    expect(() => isPalindrome(word)).toThrow();
+                  });
+                
+        
+        
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function isPalindrome(str) {
+    if (str === '') {
+        throw new Error('Input string is Empty!')
+    }
+    const st = str.toLowerCase()
+    const st2 =st.split('').reverse().join('')
+    if (st === st2) {
+        return true
+    } else {
+        return false
+    }
+}
